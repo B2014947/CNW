c1e2a8132f05d6c1347b44a66fe2e5bc0ed72b29
ThanhNam <namltgt2002@gmail.com>
2023-11-01 02:40:39 +0700

    ok

--- Client/src/components/Ad_theMenu.vue
+++ Client/src/components/Ad_theMenu.vue
@@ -2,39 +2,31 @@
   <a-menu :open-keys="openKeys" :selected-keys="selectedKeys" mode="inline">
     <a-menu-item key="admin-users">
       <router-link :to="{ name: 'admin-users' }">
-        <span> <UserOutlined class="me-2" />Accounts </span>
+        <span>
+          <font-awesome-icon class="me-2" icon="user-tie" /> Accounts
+        </span>
       </router-link>
     </a-menu-item>
 
     <a-menu-item key="admin-products">
       <router-link :to="{ name: 'admin-products' }">
-        <span> <SkinOutlined class="me-2" />Products</span>
+        <span> Products</span>
       </router-link>
     </a-menu-item>
 
     <a-menu-item key="admin-settings">
       <router-link :to="{ name: 'admin-settings' }">
-        <span> <SettingOutlined class="me-2" />Settings</span>
+        <span> <font-awesome-icon class="me-2" icon="gear" /> Settings</span>
       </router-link>
     </a-menu-item>
   </a-menu>
 </template>
 
 <script>
-import {
-  UserOutlined,
-  SkinOutlined,
-  SettingOutlined,
-} from "@ant-design/icons-vue";
 import { defineComponent } from "vue";
 import { useMenu } from "../stores/use-menu";
 import { storeToRefs } from "pinia";
 export default defineComponent({
-  components: {
-    UserOutlined,
-    SkinOutlined,
-    SettingOutlined,
-  },
   setup() {
     const stores = useMenu;
     return {
